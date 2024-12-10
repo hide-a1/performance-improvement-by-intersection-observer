2603bc8b294b8a20d7df703639770d0097e33863
hide-a1 <hide210g@gmail.com>
2024-12-10 13:33:36 +0900

    remove

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,19 +1,18 @@
-import { Component, input, viewChild } from '@angular/core';
-import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { Component, input } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Data, getData } from '../data';
 
 @Component({
-    selector: 'app-detail',
-    imports: [MatExpansionModule, MatIconModule, MatProgressSpinnerModule],
-    templateUrl: './detail.component.html',
-    styleUrl: './detail.component.scss'
+  selector: 'app-detail',
+  imports: [MatExpansionModule, MatIconModule, MatProgressSpinnerModule],
+  templateUrl: './detail.component.html',
+  styleUrl: './detail.component.scss',
 })
 export class DetailComponent {
   data = input.required<Data>();
   visible = input.required<boolean>();
-  accordion = viewChild.required(MatAccordion);
 
   detailData = getData(1000);
 }

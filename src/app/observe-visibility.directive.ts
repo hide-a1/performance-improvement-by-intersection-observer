c1530f51c27f70ea11b0bdaac8334b17d1f6f0b4
hide-a1 <hide210g@gmail.com>
2024-12-10 13:22:33 +0900

    refactor

--- src/app/observe-visibility.directive.ts
+++ src/app/observe-visibility.directive.ts
@@ -1,84 +1,63 @@
 import {
   AfterViewInit,
+  DestroyRef,
   Directive,
   ElementRef,
-  Input,
+  inject,
+  input,
   OnDestroy,
-  OnInit,
   output,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { delay, filter, Subject } from 'rxjs';
 
 @Directive({
   selector: '[observeVisibility]',
   standalone: true,
 })
-export class ObserveVisibilityDirective
-  implements OnDestroy, OnInit, AfterViewInit
-{
-  @Input() debounceTime = 0;
-  @Input() rootMargin = 0;
-  @Input() threshold = 1;
+export class ObserveVisibilityDirective implements OnDestroy, AfterViewInit {
+  private element = inject(ElementRef);
+  private destroyRef = inject(DestroyRef);
+
+  debounceTime = input(0);
+  rootMargin = input(0);
+  threshold = input(1);
 
   visible = output<boolean>();
 
   private observer: IntersectionObserver | undefined;
-  private subject$ = new Subject<
-    | {
-        entry: IntersectionObserverEntry;
-        observer: IntersectionObserver;
-      }
-    | undefined
-  >();
-
-  constructor(private element: ElementRef) {}
-
-  ngOnInit() {
-    this.createObserver();
-  }
+  private subject$ = new Subject<IntersectionObserverEntry | undefined>();
 
   ngAfterViewInit() {
-    this.startObservingElements();
-  }
-
-  ngOnDestroy() {
-    if (this.observer) {
-      this.observer.disconnect();
-      this.observer = undefined;
-    }
-
-    this.subject$.next(undefined);
-    this.subject$.complete();
-  }
-
-  private createObserver() {
     const options = {
-      rootMargin: this.rootMargin + 'px',
-      threshold: this.threshold,
+      rootMargin: this.rootMargin() + 'px',
+      threshold: this.threshold(),
     };
 
     this.observer = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
-        this.subject$.next({ entry, observer });
+        this.subject$.next(entry);
       });
     }, options);
-  }
-
-  private startObservingElements() {
-    if (!this.observer) {
-      return;
-    }
 
     this.observer.observe(this.element.nativeElement);
 
     this.subject$
-      .pipe(delay(this.debounceTime), filter(Boolean))
-      .subscribe(async ({ entry, observer }) => {
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        delay(this.debounceTime()),
+        filter((value) => value !== undefined)
+      )
+      .subscribe(async (entry) => {
         if (entry.isIntersecting && entry.intersectionRatio > 0) {
           this.visible.emit(true);
         } else {
           this.visible.emit(false);
         }
       });
+  }
+
+  ngOnDestroy() {
+    this.observer?.disconnect();
   }
 }
